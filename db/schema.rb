5d7ffcef2da5e4c9ab307b6e5b3cf30bbfd602e2
Lucas Dubernard <lucas.dubernard@hotmail.fr>
2020-01-31 14:08:43 +0100

    models seed controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_31_122952) do
+ActiveRecord::Schema.define(version: 2020_01_31_124802) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "channels", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "content"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +41,5 @@ ActiveRecord::Schema.define(version: 2020_01_31_122952) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "messages", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,62 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "==========================="
+puts "Destroy seed"
+puts "==========================="
+
+Message.destroy_all
+User.destroy_all
+Channel.destroy_all
+
+puts "==========================="
+puts "Seed destroyed !!!"
+puts "==========================="
+
+
+# Channel
+puts "==========================="
+puts "Creating Channels"
+puts "==========================="
+
+paris = Channel.new(name:"Paris")
+general = Channel.new(name:"General")
+react = Channel.new(name:"React")
+
+paris.save!
+general.save!
+react.save!
+
+
+# Users
+puts "==========================="
+puts "Creating Users"
+puts "==========================="
+
+valentin = User.new(email:"valentin@gmail.com", password: "azerty")
+
+paul = User.new(email:"paul@gmail.com", password: "azerty")
+
+lucas = User.new(email:"lucas@gmail.com", password: "azerty")
+
+valentin.save!
+paul.save!
+lucas.save!
+
+
+# Messages
+puts "==========================="
+puts "Creating Messages"
+puts "==========================="
+
+message1 = Message.new(user: valentin, channel: paris, content: "Je suis val et je suis dans Paris")
+message2 = Message.new(user: paul, channel: paris, content: "ZZZZZ")
+message3 = Message.new(user: lucas, channel: general, content: "Lucas dans le géneral")
+message4 = Message.new(user: lucas, channel: react, content: "Lucas dans react")
+
+message1.save!
+message2.save!
+message3.save!
+message4.save!
+
+
+puts "==========================="
+puts "OK OK OK !!!!!"
+puts "==========================="
